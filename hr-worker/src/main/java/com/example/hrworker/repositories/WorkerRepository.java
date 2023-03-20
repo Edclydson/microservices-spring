2df0643ddc4c0a8c5fa2ca0c2ad4043d8c43531d
Edclydson <edclydson.sousa@gmail.com>
2023-03-20 20:04:11 -0300

    :sparkles: feat(WorkerRepository): Criando Repositório de Worker

---
+++ hr-worker/src/main/java/com/example/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,11 @@
+package com.example.hrworker.repositories;
+
+import com.example.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface WorkerRepository extends JpaRepository<Worker, UUID>{
+}
