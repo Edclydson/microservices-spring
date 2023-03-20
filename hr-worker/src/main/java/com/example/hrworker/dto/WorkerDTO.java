0e6d7cc2446f159683068e2490c23c78d8c4a7a1
Edclydson <edclydson.sousa@gmail.com>
2023-03-20 20:00:33 -0300

    :sparkles: feat(WorkerDTO): Criando DTO de Worker

---
+++ hr-worker/src/main/java/com/example/hrworker/dto/WorkerDTO.java
@@ -0,0 +1,11 @@
+package com.example.hrworker.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class WorkerDTO{
+    private String name;
+    private Double dailyIncome;
+}
